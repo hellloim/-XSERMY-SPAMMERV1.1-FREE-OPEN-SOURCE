7ba43ff4345f8809b519b9f42c6483f3688995a7
SERKI#0463 (hackshub) <79936930+hellloim@users.noreply.github.com>
2022-06-03 18:31:34 +0200

    Create users.bat

---
+++ users.bat
@@ -0,0 +1,16 @@
+@echo off
+echo "username password"
+set/users.bat=username password"
+:startup
+"users.bat"
+echo (self.ip != None and self.port != None):
+echo starting!
+ def Login(self):
+        if (self.isproxyok != True):
+            return
+set/"username": self.user,
+            "password": self.password,
+            "queryParams": "{}",
+            "optIntoOneTap": "false"
+            
+            // 
